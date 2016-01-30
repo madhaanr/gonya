9f5068936e4d3dd3f96f72afc341f67c76e9fe3b
Marko Haanranta <marko.haanranta@helsinki.fi>
2016-01-30 18:27:28 +0200

    firebase saves and loads the name but not the image yet

--- app/image-form.component.ts
+++ app/image-form.component.ts
@@ -1,30 +1,54 @@
 import {Component} from 'angular2/core';
 import {NgForm} from 'angular2/common';
 import {Image} from './image';
+import {FirebaseEventPipe} from './firebasepipe';
 
 @Component({
     selector: 'image-form',
     templateUrl: 'app/image-form.component.html',
+    pipes: [FirebaseEventPipe]
 })
 export class ImageFormComponent {
-    model = new Image(18, 'Dr IQ', Date.now());
-    submitted = false;
-    onSubmit() { this.submitted = true; }
-    // TODO: Remove this when we're done
+    /*model = new Image(18, 'Dr IQ', Date.now());
+    submitted = false;*/
+    firebaseUrl: string;
+	messagesRef: Firebase;
+    constructor() {
+		this.firebaseUrl = "https://gonya.firebaseio.com/messages";
+		this.messagesRef = new Firebase(this.firebaseUrl);
+		/*this.messagesRef.onAuth((user) => {
+			if (user) {
+				this.authData = user;
+				this.isLoggedIn = true;
+			}
+		});*/
+	}
+    //onSubmit() { this.submitted = true; }
     previewFile() {
         var preview = (<HTMLInputElement>document.querySelector('img'));
         var file = (<HTMLInputElement>document.querySelector('input[type=file]')).files[0];
         var reader = new FileReader();
- 
-        reader.addEventListener("load", function() {
-            preview.src=reader.result;
-        }, false);
+        if (/\.(jpe?g|png|gif)$/i.test(file.name)) {
+            reader.addEventListener("load", function() {
+                preview.src = reader.result;
+            }, false);
 
-        if (file) {
-            reader.readAsDataURL(file);
+            if (file) {
+                reader.readAsDataURL(file);
+            }
         }
     }
-    get diagnostic() { return JSON.stringify(this.model); }
+    doneTyping($event) {
+	  if($event.which === 13) {
+	    this.addMessage($event.target.value);
+	    $event.target.value = null;
+	  }
+	}
+    addMessage(message: string) {
+		this.messagesRef.push({
+			name: message
+		});
+	}
 }
 /*function previewFile() {
     var preview = (<HTMLInputElement>document.querySelector('img'));
